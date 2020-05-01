8f4a5026d539be78d83ffcc3a6e95463dfdf1c70
jakesev <jaysev92@gmail.com>
2020-05-01 17:24:25 +1000

    commit on master

--- sortArray.js
+++ sortArray.js
@@ -3,4 +3,14 @@ sortArray([5, 3, 2, 8, 1, 4])
 function sortArray(array){
     
     
+    
+}
+
+function swap(items, firstIndex, secondIndex){
+
+
+}
+
+function partition(items,  left, right){
+
 }
