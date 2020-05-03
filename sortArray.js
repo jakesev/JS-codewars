5d539b840ab28bb6cafc281888108eadb998c659
jakesev <jaysev92@gmail.com>
2020-05-03 20:29:15 +1000

    codewars solutions

--- sortArray.js
+++ sortArray.js
@@ -2,15 +2,62 @@ sortArray([5, 3, 2, 8, 1, 4])
 
 function sortArray(array){
     
+
+   var result =  quickSort(array)
     
     
 }
 
-function swap(items, firstIndex, secondIndex){
+function quickSort(array, left, right){
+    var index;
+    if (array.length > 1){
+        left = typeof left != "number" ? 0 : left;
+        right = typeof right != "number" ? array.length - 1: right;
 
+        index = partition (array, left, right);
+        console.log(left+" " + index) 
+        if(left < index - 1){
+                quickSort(array, left, index -1)
+        }
+        console.log(index+" " + right) 
 
+        if(index < right ){
+            quickSort(array,index, right)
+        }
+    }
+
+    return array;
 }
 
-function partition(items,  left, right){
+function swap(array, firstIndex, secondIndex){
+    var temp = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = temp;
+}
 
+function partition(array,  left, right){
+    var pivot = array[Math.floor((right + left) / 2)],
+    i = left,
+    j = right;
+
+   //while i <= j continue the loop
+    while(i<=j){
+
+        while(array[i] < pivot){
+            i++;
+        }
+        
+        while(array[j] > pivot){
+            j--;
+        }
+
+        // Swap position of i and j if i<=j
+        if(i<=j){
+            swap(array,i,j);
+            i++;
+            j--;
+        }
+        console.log("Making it here")
+    }
+    return i;
 }
